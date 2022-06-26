97076a54bbdbf9be76f31dbb93d8a91256dd50c7
Federico Carballo <ganimedes2q@gmail.com>
2022-06-25 22:56:46 -0300

    arreglo de errores

--- js/variables.js
+++ js/variables.js
@@ -1,8 +1,8 @@
+
 const inputEmail = document.getElementById("email-registro");
 const inputPassword = document.getElementById("password-registro");
 const inputName = document.getElementById("nombre-registro");
 const inputText = document.getElementById("usuario-registro");
 const inputSubmit = document.getElementById("boton-submit");
-
-const inputEmailLogin = document.querySelector("#email-login")
-const inputPasswordLogin = document.querySelector("#password-login")
+const inputEmailLogin = document.querySelector("#email-login");
+const inputPasswordLogin = document.querySelector("#password-login");
