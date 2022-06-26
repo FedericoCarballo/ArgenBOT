0acb99f09d805094cb445ca7be8950ab848cc2b1
Federico Carballo <ganimedes2q@gmail.com>
2022-06-25 22:46:06 -0300

    Entrega de la 2da preentrega del proyecto final

--- js/productos.js
+++ js/productos.js
@@ -1,16 +1,3 @@
-const DESCUENTOPRIMERACOMPRA = 20
-
-const producto1 = new Producto("Torta Brownie", 10, 2200, DESCUENTOPRIMERACOMPRA)
-const producto2 = new Producto("Lemon Pie", 4, 2100, DESCUENTOPRIMERACOMPRA)
-const producto3 = new Producto("CheeseCake", 2, 2470, DESCUENTOPRIMERACOMPRA)
-const producto4 = new Producto("Hudson", 6, 2470, DESCUENTOPRIMERACOMPRA)
-const producto5 = new Producto("Bauhaus", 2, 2200, DESCUENTOPRIMERACOMPRA)
-const producto6 = new Producto("BauhausSM", 10, 2200, DESCUENTOPRIMERACOMPRA)
-
-
-
-
-
 const producto = [
     {nombre: "Torta Brownie", precio: 2200},
     {nombre: "Lemon Pie", precio: 2100},
@@ -23,7 +10,6 @@ const producto = [
 const productos = producto.filter((el) => {return el.nombre.includes("Bauhaus")})
 //const productos2 = producto.filter((el) => el.nombre < 2200)
 console.log(productos)
-
 
 let tortaFavorita = prompt("Ingrese su torta favorita");
 let torta = producto.find ((el) => {return el.nombre == tortaFavorita})
--- js/registro.js
+++ js/registro.js
@@ -1,16 +1,21 @@
-const email = document.getElementById("email-registro");
-const password = document.getElementById("password-registro");
-const name = document.getElementById("nombre-registro");
-const text = document.getElementById("usuario-registro");
 
-let miFormulario = document.getElementById("formulario");
-miFormulario.addEventListener("submit", validarFormulario);
-
-function validarFormulario(e) {
+document.addEventListener("submit", (e) => {
     e.preventDefault();
+    guardarDatos();
     console.log("Formulario Enviado");
-    alert("Bienvenido a Scarlett - Delicias Finas")
+    alert("Registro completado. Bienvenido a Scarlett - Delicias Finas")
+})
+
+
+function guardarDatos () {
+    const datosGuardados = {nombre:inputName.value,
+                            usuario:inputText.value,
+                            email:inputEmail.value,
+                            contraseña:inputPassword.value}
+    let str = JSON.stringify(datosGuardados)
+    sessionStorage.setItem("datosGuardados", str)
 }
+
 
 let halagoBienvenida = ["Gracias por elegirnos", "Esperamos tu compra :)", "No olvides ver nuestro catalogo", "Tenemos alta variedad de productos", "Deleita a tu familia con nuestros exquisitos sabores"];
 const bienvenida = halagoBienvenida[Math.floor(Math.random() * halagoBienvenida.length)];
